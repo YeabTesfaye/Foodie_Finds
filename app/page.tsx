983fff32b832ee808cce515cbce42fb87c47e222
YeabTesfaye <tesfayeyeabsra446@gmail.com>
2024-10-05 20:13:40 +0300

    add an image slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import { Fragment } from "react"; 
-
 import classes from "./page.module.css"; 
-
+import ImageSlideshow from '@/components/images/image-slideshow'
 export default function Home() {
   return (
     <Fragment>
       <header className={classes.header}>
-        <div className={classes.slideshow}></div>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
+        </div>
         <div>
           <div className={classes.hero}>
             <h1>NextLevel Food for NextLevel Foodies</h1>
